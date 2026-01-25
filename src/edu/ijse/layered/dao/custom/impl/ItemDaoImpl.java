7249d94c09f23cd876da64c504d917df4ba8adb5
Nethsara Vimukthi <nethsaravimukthiinduwara@gmail.com>
2026-01-25 22:45:02 +0530

    CRUD DAO

--- src/edu/ijse/layered/dao/custom/impl/ItemDaoImpl.java
+++ src/edu/ijse/layered/dao/custom/impl/ItemDaoImpl.java
@@ -5,11 +5,38 @@
 package edu.ijse.layered.dao.custom.impl;
 
 import edu.ijse.layered.dao.custom.ItemDao;
+import edu.ijse.layered.entity.ItemEntity;
+import java.util.ArrayList;
 
 /**
  *
  * @author neths
  */
 public class ItemDaoImpl implements ItemDao{
+
+    @Override
+    public boolean add(ItemEntity t) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean update(ItemEntity t) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        return false;
+    }
+
+    @Override
+    public ItemEntity get(String id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ArrayList<ItemEntity> getAll() throws Exception {
+        return null;
+    }
     
 }
